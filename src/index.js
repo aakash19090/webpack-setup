dbf9f9b5e84b30378f4bc3b209b0212e4967153c
Akash Sharma <akash.sharma@o2h.com>
2021-02-28 09:53:31 +0530

    minimal webpack configuration &  added babel support

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const user = {
+    firstName: "Akash",
+    lastName: "Sharma",
+    age: 28,
+}
+
+const user2 ={
+    ...user,
+    email: 'aakashsharma@gmail.com'
+}
+
+console.log(user);
+console.log(user2);
